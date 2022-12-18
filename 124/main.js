8cd642271c702cade79cbda266d52ed748b36562
Maharshi Patel <88446251+maharshi878@users.noreply.github.com>
2022-12-18 10:31:33 +0530

    Add files via upload

--- 124/main.js
+++ 124/main.js
@@ -20,3 +20,30 @@ function modelLoaded() {
 function draw() {
   background('#808080');
 }
+
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("nouseX = "+ noseX +"noseY ="+ noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristY = results[0].pose.leftWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    }
+}
+function draw() {
+  background('#969A97');
+  document.getElementById("square_side").innerHTML = "Width And Height of the square will be = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
+}
