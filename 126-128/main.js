47a47a53987073a89d08bf2bfa1dfb4e55373592
Maharshi Patel <88446251+maharshi878@users.noreply.github.com>
2023-03-14 10:56:03 +0530

    Update main.js

--- 126-128/main.js
+++ 126-128/main.js
@@ -1,14 +1,18 @@
 song = "";
-leftWristX = 0;
-leftWristY = 0;
-rightWristX = 0;
-rightWristY = 0;
-scoreLeftWrist = 0;
-scoreRightWrist = 0;
+
 function preload(){
   song = loadSound("music.mp3");
-
 }
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
 function setup() {
   canvas = createCanvas(600, 500);
   canvas.center();
