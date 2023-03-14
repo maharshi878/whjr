b22f7c510a110470ad8c1bca988163ccd76c1823
Maharshi Patel <88446251+maharshi878@users.noreply.github.com>
2023-03-14 10:42:14 +0530

    Update main.js

--- 126-128/main.js
+++ 126-128/main.js
@@ -25,6 +25,9 @@ function modelLoaded() {
 function gotPoses(results) {
   if(results.length > 0) {
     console.log(results);
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("scoreLWrist = "+ scoreLeftWrist);
+
     leftWristX = results[0].pose.leftWrist.x
     leftWristY = results[0].pose.leftWrist.y
     console.log(" l x="+leftWristX+" l y="+leftWristY);
@@ -36,6 +39,18 @@ function gotPoses(results) {
 
 function draw() {
   image(video,0,0,600,500);
+
+  fill('#FF0000');
+  stroke('#FF0000');
+
+  if(scoreLeftWrist > 0.2) {
+  circle(leftWristX,leftWristY,20);
+  InNumberleftWristY = Number(leftWristY);
+  remove_decimals = floor(InNumberleftWristY);
+  volume = remove_decimals/500;
+  document.getElementById("volume").innerHTML = "Volume = " + volume;
+  song.setVolume(volume);
+  }
 }
 function play() {
   song.play();
