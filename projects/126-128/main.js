36be2cebf67a63dac5c6b7b97c3c7005fee4db32
Maharshi Patel <88446251+maharshi878@users.noreply.github.com>
2023-03-27 10:48:12 +0530

    Update main.js

--- projects/126-128/main.js
+++ projects/126-128/main.js
@@ -1,7 +1,13 @@
 song = "";
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 function preload(){
-  song = loadSound("music.mp3");
+  song1 = loadSound("music1.mp3");
+  song1 = loadSound("music2.mp3");
 }
 function setup() {
   canvas = createCanvas(600, 500);
@@ -9,11 +15,52 @@ function setup() {
 
   video = createCapture(VIDEO);
   video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+  console.log('poseNet is initialized');
+}
+
+function gotPoses(results) {
+  if(results.length > 0) {
+    console.log(results);
+    scoreRightWrist = results[0].pose.keypoints[10].score;
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("scoreRWrist" + scoreRightWrist + " scoreLWrist = " + scoreLeftWrist);
+    leftWristX = results[0].pose.leftWrist.x
+    leftWristY = results[0].pose.leftWrist.y
+    console.log(" l x="+leftWristX+" l y="+leftWristY);
+    rightWristX = results[0].pose.rightWrist.x
+    rightWristY = results[0].pose.rightWrist.y
+    console.log(" r x="+rightWristX+" r y="+rightWristY);
+  }
 }
 
 function draw() {
   image(video,0,0,600,500);
+
+  fill('#FF0000');
+  stroke('#FF0000');
+  circle(rightWristX,rightWristY,20);
+
+  if(rightWristY > 0) {
+   play1();
+  }
+  if(leftwristY > 0) {
+   play2();
+  }
 }
-function play() {
-  song.play();
+
+function play1() {
+  song1.play();
+  song1.setVolume(1);
+  song1.rate(1);
+}
+function play2() {
+  song2.play();
+  song2.setVolume(1);
+  song2.rate(1);
 }
