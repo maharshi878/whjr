c665f286f192678be9da194d698b38511224af6d
Maharshi Patel <88446251+maharshi878@users.noreply.github.com>
2023-03-27 10:25:23 +0530

    Update main.js

--- 126-128/main.js
+++ 126-128/main.js
@@ -1,18 +1,14 @@
 song = "";
-
-function preload(){
-  song = loadSound("music.mp3");
-}
-
-scoreRightWrist = 0;
-scoreLeftWrist = 0;
-
-rightWristX = 0;
-rightWristY = 0;
-
 leftWristX = 0;
 leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+function preload(){
+  song = loadSound("music.mp3");
 
+}
 function setup() {
   canvas = createCanvas(600, 500);
   canvas.center();
@@ -31,8 +27,9 @@ function modelLoaded() {
 function gotPoses(results) {
   if(results.length > 0) {
     console.log(results);
+    scoreRightWrist = results[0].pose.keypoints[10].score;
     scoreLeftWrist = results[0].pose.keypoints[9].score;
-    console.log("scoreLWrist = "+ scoreLeftWrist);
+    console.log("scoreRWrist" + scoreRightWrist + " scoreLWrist = " + scoreLeftWrist);
 
     leftWristX = results[0].pose.leftWrist.x
     leftWristY = results[0].pose.leftWrist.y
@@ -49,6 +46,30 @@ function draw() {
   fill('#FF0000');
   stroke('#FF0000');
 
+  if(scoreRightWrist > 0.2) {
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristY >0 && rightWristY <= 100){
+      document.getElementById("speed").innerHTML = "Speed = 1/2x";
+      song.rate(0.5);
+    }
+    else if(rightWristY >100 && rightWristY <= 200){
+      document.getElementById("speed").innerHTML = "Speed = 1x";
+      song.rate(1);
+    }
+    else if(rightWristY >200 && rightWristY <= 300){
+      document.getElementById("speed").innerHTML = "Speed = 1.5x";
+      song.rate(1.5);
+    }
+    else if(rightWristY >300 && rightWristY <= 400){
+      document.getElementById("speed").innerHTML = "Speed = 1.75x";
+      song.rate(1.75);
+    }
+    else if(rightWristY >400 && rightWristY <= 500){
+      document.getElementById("speed").innerHTML = "Speed = 2x";
+      song.rate(2);
+    }
+  }
   if(scoreLeftWrist > 0.2) {
   circle(leftWristX,leftWristY,20);
   InNumberleftWristY = Number(leftWristY);
