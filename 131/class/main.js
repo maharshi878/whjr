1e3f822435e388ea26adf231c2968d383ec48537
Maharshi Patel <88446251+maharshi878@users.noreply.github.com>
2023-05-02 10:53:52 +0530

    Add files via upload

--- 131/class/main.js
+++ 131/class/main.js
@@ -43,11 +43,11 @@ function draw() {
         for (var i = 0; i < objects.length; i++) {
           document.getElementById("status").innerHTML = "Status : Object Detected";
           document.getElementById("number_of_objects").innerHTML = "Number of objects detected : "+objects.length;
-          fill(r, g, b);
+          fill("#FF0000");
           percent = floor(objects[i].confidence * 100);
           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
           noFill();
-          stroke(r, g, b);
+          stroke("#FF0000");
           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
       }
